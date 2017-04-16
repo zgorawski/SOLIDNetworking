85b315e580b8b9ab08d4e8567cbc8f02420bf3ec
Zbigniew Górawski <zgorawski@gmail.com>
2017-04-16 09:40:21 +0200

    Initial Commit

---
+++ SOLIDNetworking/SOLIDNetworking.h
@@ -0,0 +1,19 @@
+//
+//  SOLIDNetworking.h
+//  SOLIDNetworking
+//
+//  Created by Zbigniew Górawski on 16/04/2017.
+//  Copyright © 2017 Zbigniew Górawski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SOLIDNetworking.
+FOUNDATION_EXPORT double SOLIDNetworkingVersionNumber;
+
+//! Project version string for SOLIDNetworking.
+FOUNDATION_EXPORT const unsigned char SOLIDNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SOLIDNetworking/PublicHeader.h>
+
+
